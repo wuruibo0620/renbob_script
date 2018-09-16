d85ce9fafd8f5946b3cbdaa69a8dea56ca55914f
wuruibo0620 <baiaohao@126.com>
2018-09-16 21:19:34 +0800

    二叉树增加了"后续遍历方法(非递归)"

--- script/text_demo.py
+++ script/text_demo.py
@@ -43,10 +43,36 @@ class Tree(object):  # 定义一个二叉树对象
                     q.append(pop_node.child2)
         return res
 
+    def preorder(self,root):    # 定义一个先序遍历方法
+        if root == None:
+            return []
+        else:
+            rootItem = [root.item]
+            leftItem = self.preorder(root.child1)
+            rightItem = self.preorder(root.child2)
+        return rootItem + leftItem + rightItem
+
+    def postorder(self,root):
+        if root == None:
+            return []
+        else:
+            res = []
+            stack = []
+            pop_node = root
+            while pop_node or stack:
+                while pop_node:
+                    stack.append(pop_node)
+                    res.append(pop_node.item)
+                    pop_node = pop_node.child2
+                if stack:
+                    pop_node = stack.pop()
+                    pop_node = pop_node.child1
+        return res[::-1]
+
 
 if __name__ == '__main__':
     tree = Tree()
     for i in range(1,8):
         tree.add(i)
 
-    print(tree.traverse())
+    print(tree.postorder(tree.root))
